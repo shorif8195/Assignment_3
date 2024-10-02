58370d0991843b76a90e3d164bc1a1ae073968c4
Shorif Ahmad <shorif8195@gmail.com>
2024-10-02 23:43:02 +0600

    second time update

--- app/src/main/java/com/example/assignment_3/MainActivity.java
+++ app/src/main/java/com/example/assignment_3/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.assignment_3;
 
 import android.os.Bundle;
 import android.text.Layout;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -18,9 +19,9 @@ import java.util.regex.Pattern;
 public class MainActivity extends AppCompatActivity {
     LinearLayout first_layout,second_layout;
     private TextView outputText;
-    private EditText eamil,pass;
+    private EditText email,pass;
     private Button login;
-    private String name,password;
+    private String em,password;
     private Pattern emailPattern = Pattern.compile("[a-zA-z]");
     private Pattern passPattern = Pattern.compile("^(?=.*[a-z])(?=.*\\d)[a-zA-Z\\d]{6,}$");
 
@@ -29,6 +30,65 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        email =(EditText) findViewById(R.id.email_id);
+        pass =(EditText) findViewById(R.id.pass_id);
+        login = (Button)findViewById(R.id.submit_id);
+
+        first_layout = findViewById(R.id.layout_1);
+        second_layout = findViewById(R.id.layout_2);
+        outputText = findViewById(R.id.output_text);
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                em = email.getText().toString();
+                password = pass.getText().toString();
+
+                if(em.isEmpty()){
+                    pass.setError("Empty !!.");
+                    pass.requestFocus();
+                    return;
+
+                }
+                else if(!emailPattern.matcher(em).matches()){
+                    email.setError("Enter a valid email. ");
+                    pass.requestFocus();
+                    return;
+
+                };
+
+                if(password.isEmpty()){
+                    pass.setError("Empty.");
+                    pass.requestFocus();
+                    return;
+                }
+                else if(!passPattern.matcher(password).matches()){
+                    pass.setError("Use all type of character .");
+                    pass.requestFocus();
+                    return;
+                };
+
+                first_layout.setVisibility(View.GONE);
+                second_layout.setVisibility(View.VISIBLE);
+                outputText.setText("Welcome.\n Log in successfully.");
+
+
+
+
+
+
+
+
+            }
+        });
+
+
+
+
+
+
 
     }
 }
